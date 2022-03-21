204cb0d1262b1f685b1ab1f5bd8dab59b7df070f
AriNoa <arikawa.nao15@gmail.com>
2022-03-21 14:21:45 +0900

    🏷️ update dto

--- domain/protocol/tag.go
+++ domain/protocol/tag.go
@@ -1,17 +1,21 @@
 package protocol
 
-type TagID string
-
 type Tag struct {
+	id   string
 	name string
+}
+
+func (t Tag) Id() string {
+	return t.name
 }
 
 func (t Tag) Tag() string {
 	return t.name
 }
 
-func NewTag(name string) Tag {
+func NewTag(id string, name string) Tag {
 	return Tag{
+		id:   id,
 		name: name,
 	}
 }
--- domain/protocol/task.go
+++ domain/protocol/task.go
@@ -1,12 +1,15 @@
 package protocol
 
-type TaskID string
-
 type Task struct {
+	id          string
 	title       string
 	description string
-	tags        []TaskID
+	tags        []string
 	completed   bool
+}
+
+func (t Task) Id() string {
+	return t.id
 }
 
 func (t Task) Title() string {
@@ -17,8 +20,8 @@ func (t Task) Description() string {
 	return t.description
 }
 
-func (t Task) Tags() []TaskID {
-	return append([]TaskID{}, t.tags...)
+func (t Task) Tags() []string {
+	return append([]string{}, t.tags...)
 }
 
 func (t Task) Completed() bool {
@@ -26,12 +29,14 @@ func (t Task) Completed() bool {
 }
 
 func NewTask(
+	id string,
 	title string,
 	description string,
-	tags []TaskID,
+	tags []string,
 	completed bool,
 ) Task {
 	return Task{
+		id:          id,
 		title:       title,
 		description: description,
 		tags:        tags,
