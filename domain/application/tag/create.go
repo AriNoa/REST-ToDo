98b8182ec84134efe184dc1539716b033434700c
AriNoa <arikawa.nao15@gmail.com>
2022-03-22 23:07:02 +0900

    ✅ impl CreateInteractor

--- domain/application/tag/create.go
+++ domain/application/tag/create.go
@@ -1,7 +1,9 @@
 package tag
 
 import (
+	"github.com/AriNoa/REST-ToDo/domain/model/ids"
 	model "github.com/AriNoa/REST-ToDo/domain/model/tag"
+	"github.com/AriNoa/REST-ToDo/domain/protocol"
 	usecase "github.com/AriNoa/REST-ToDo/domain/usecase/tag/create"
 )
 
@@ -10,7 +12,22 @@ type CreateInteractor struct {
 }
 
 func (m *CreateInteractor) Handle(data usecase.InputData) (usecase.OutputData, error) {
-	return usecase.OutputData{}, nil
+	name, err := model.NewName(data.Name())
+
+	if err != nil {
+		return usecase.OutputData{}, err
+	}
+
+	tag, err := m.repository.Create(name)
+
+	if err != nil {
+		return usecase.OutputData{}, err
+	}
+
+	return usecase.NewOutputData(
+		protocol.NewTag(ids.String(tag.Id()), tag.Name().String()),
+	), nil
+
 }
 
 func NewCreateInteractor(repository model.Repository) *CreateInteractor {
