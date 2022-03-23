64410a27d939e7674633cbc3dede72f2a37c7ebf
AriNoa <arikawa.nao15@gmail.com>
2022-03-23 18:43:53 +0900

    🥅 add tag already created error

--- domain/protocol/errors.go
+++ domain/protocol/errors.go
@@ -3,9 +3,10 @@ package protocol
 import "errors"
 
 var (
-	ErrTagNotFound  = errors.New("tag not found")
-	ErrTaskNotFound = errors.New("task not found")
-	ErrNoTagName    = errors.New("no tag name error")
-	ErrNoTaskTitle  = errors.New("no task title error")
-	ErrInternal     = errors.New("internal error")
+	ErrTagNotFound       = errors.New("tag not found")
+	ErrTaskNotFound      = errors.New("task not found")
+	ErrNoTagName         = errors.New("no tag name error")
+	ErrNoTaskTitle       = errors.New("no task title error")
+	ErrTagAlreadyCreated = errors.New("tag already created")
+	ErrInternal          = errors.New("internal error")
 )
